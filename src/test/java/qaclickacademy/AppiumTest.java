8893aac35290150d7fa100deebc29d4665335407
Alexander <Alexanderschlee92@hotmail.de>
2020-02-12 16:22:49 +0100

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeApAndroid ()
+	{
+		System.out.println("NativeApAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	//testng xml file from Maven
+	//how to execute all tets cases from test folder - mvn test
+@Test
+public void BrowserAutomation () 
+{
+System.out.println("BrowserAutomation");
+	}
+
+@Test
+public void elementsUI()
+{
+	System.out.println("elementsUI");
+	
+}
+}
+
